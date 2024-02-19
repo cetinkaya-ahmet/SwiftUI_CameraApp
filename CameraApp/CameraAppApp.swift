ad0d33b42f64bb0e3bafa2cef885c0c6da7430b7
ahmetcetinkaya <4hmetcetinkaya@gmail.com>
2024-02-19 17:05:48 +0300

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by Ahmet Çetinkaya on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
